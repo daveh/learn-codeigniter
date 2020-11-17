07fec68b1dd858771b27a41e506dfb7d0580783e
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 15:13:03 +0100

    Initial commit

---
+++ app/Views/Tasks/form.php
@@ -0,0 +1,5 @@
+<div>
+    <label for="description">Description</label>
+    <input type="text" name="description" id="description"
+           value="<?= old('description', esc($task['description'])) ?>">
+</div>
