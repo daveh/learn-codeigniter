2dc147435fb7eecc2f9342ffc7a93c373247eb8b
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 15:18:06 +0100

    Initial commit

---
+++ app/Controllers/Tasks.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Entities\Task;
+
+class Tasks extends BaseController
+{
+	private $model;
+	
+	public function __construct()
+	{
+        $this->model = new \App\Models\TaskModel;
+	}
+	
+	public function index()
+	{
+        $data = $this->model->findAll();
+		
+		return view("Tasks/index", ['tasks' => $data]);
+	}
+	
+	public function show($id)
+    {
+        $task = $this->model->find($id);
+		
+		return view('Tasks/show', [
+            'task' => $task
+        ]);
+	}
+	
+	public function new()
+	{
+        $task = new Task;
+		
+		return view('Tasks/new', [
+		    'task' => $task
+        ]);
+	}
+	
+	public function create()
+	{
+        $task = new Task($this->request->getPost());
+		
+		if ($this->model->insert($task)) {
+
+			return redirect()->to("/tasks/show/{$this->model->insertID}")
+							 ->with('info', 'Task created successfully');
+		
+        } else {
+
+			return redirect()->back()
+							 ->with('errors', $this->model->errors())
+							 ->with('warning', 'Invalid data')
+							 ->withInput();
+		}
+	}
+	
+	public function edit($id)
+	{
+		$task = $this->model->find($id);
+		
+		return view('Tasks/edit', [
+            'task' => $task
+        ]);
+	}
+	
+    public function update($id)
+	{
+        $task = $this->model->find($id);
+		
+		$task->fill($this->request->getPost());
+		
+		if ( ! $task->hasChanged()) {
+			
+            return redirect()->back()
+                             ->with('warning', 'Nothing to update')
+                             ->withInput();
+		}
+		
+        if ($this->model->save($task)) {
+				
+	        return redirect()->to("/tasks/show/$id")
+	                         ->with('info', 'Task updated successfully');
+							 
+		} else {
+			
+            return redirect()->back()
+                             ->with('errors', $this->model->errors())
+                             ->with('warning', 'Invalid data')
+							 ->withInput();
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
