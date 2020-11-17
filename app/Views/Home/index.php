aa6c22bb71ef856c9c33c763cc8fbdfa2785c02f
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 14:39:10 +0100

    Initial commit

---
+++ app/Views/Home/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("content") ?>
+
+    <h1>Welcome</h1>
+
+<?= $this->endSection() ?>
---
+++ app/Views/Tasks/index.php
@@ -0,0 +1,6 @@
+<?= $this->include("header") ?>
+
+    <h1>Tasks</h1>
+
+</body>
+</html>
