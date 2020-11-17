c8eb3e9b7425b7e1077bff2bcff586fb13e9cd9c
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 15:03:59 +0100

    Initial commit

---
+++ app/Controllers/Tasks.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controllers;
+
+class Tasks extends BaseController
+{
+	public function index()
+	{
+        $model = new \App\Models\TaskModel;
+        $data = $model->findAll();
+		
+		return view("Tasks/index", ['tasks' => $data]);
+	}
+	
+	public function show($id)
+    {
+        $model = new \App\Models\TaskModel;
+		
+        $task = $model->find($id);
+		
+		return view('Tasks/show', [
+            'task' => $task
+        ]);
+	}
+	
+	public function new()
+	{
+		return view('Tasks/new');
+	}
+	
+	public function create()
+	{
+        $model = new \App\Models\TaskModel;
+		
+		$result = $model->insert([
+			'description' => $this->request->getPost("description")
+		]);
+		
+        if ($result === false) {
+			
+            return redirect()->back()
+							 ->with('errors', $model->errors())
+							 ->with('warning', 'Invalid data');
+		
+        } else {
+		
+			return redirect()->to("/tasks/show/$result")
+							 ->with('info', 'Task created successfully');
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
