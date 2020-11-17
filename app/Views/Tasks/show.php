9a4476235bb57593e3910cd9aa081cac84b9a7e2
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 14:54:47 +0100

    Initial commit

---
+++ app/Views/Tasks/show.php
@@ -0,0 +1,23 @@
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('title') ?>Task<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+<h1>Task</h1>
+
+<dl>
+    <dt>ID</dt>
+    <dd><?= $task['id'] ?></dd>
+    
+    <dt>Description</dt>
+    <dd><?= $task['description'] ?></dd>
+    
+    <dt>Created at</dt>
+    <dd><?= $task['created_at'] ?></dd>
+    
+    <dt>Updated at</dt>
+    <dd><?= $task['updated_at'] ?></dd>
+</dl>
+
+<?= $this->endSection() ?>
