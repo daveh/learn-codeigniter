2ae36995c843eadab0cfedbd39df2798b2d40c77
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 21:12:25 +0100

    Initial commit

---
+++ app/Views/Admin/Users/form.php
@@ -0,0 +1,19 @@
+<div>
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" value="<?= old('name') ?>">
+</div>
+
+<div>
+    <label for="email">email</label>
+    <input type="text" name="email" id="email" value="<?= old('email') ?>">
+</div>
+
+<div>
+    <label for="password">Password</label>
+    <input type="password" name="password">
+</div>
+
+<div>
+    <label for="password_confirmation">Repeat password</label>
+    <input type="password" name="password_confirmation">
+</div>
