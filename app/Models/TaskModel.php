8b600e6c58a272a844b9077bfb511682c76a08ae
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 15:21:44 +0100

    Initial commit

---
+++ app/Models/TaskModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+class TaskModel extends \CodeIgniter\Model
+{
+    protected $table = 'task';
+    
+    protected $allowedFields = ['description'];
+    
+    protected $returnType = 'App\Entities\Task';
+    
+    protected $useTimestamps = true;
+    
+    protected $validationRules = [
+        'description' => 'required'
+    ];
+    
+    protected $validationMessages = [
+        'description' => [
+            'required' => 'Please enter a description'
+        ]
+    ];
+}
