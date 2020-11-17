204e6609fe8a1460f38c40215f8f4c32bc8ec899
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-17 14:57:41 +0100

    Initial commit

---
+++ app/Controllers/Tasks.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controllers;
+
+class Tasks extends BaseController
+{
+	public function index()
+	{
+        $model = new \App\Models\TaskModel;
+        $data = $model->findAll();
+		
+		return view("Tasks/index", ['tasks' => $data]);
+	}
+	
+	public function show($id)
+    {
+        $model = new \App\Models\TaskModel;
+		
+        $task = $model->find($id);
+		
+		return view('Tasks/show', [
+            'task' => $task
+        ]);
+	}
+	
+	public function new()
+	{
+		return view('Tasks/new');
+	}
+	
+	public function create()
+	{
+        $model = new \App\Models\TaskModel;
+		
+		$model->insert([
+			'description' => $this->request->getPost("description")
+		]);
+		
+		dd($model->insertID);
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ app/Models/TaskModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class TaskModel extends \CodeIgniter\Model
+{
+    protected $table = 'task';
+    
+    protected $allowedFields = ['description'];
+}
