411bd66410cba9456b5710e582d8b7977200a659
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 06:55:23 +0100

    Initial commit

---
+++ app/Controllers/Signup.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace App\Controllers;
+
+class Signup extends BaseController
+{
+    public function new()
+    {
+		return view("Signup/new");
+    }
+    
+    public function create()
+    {
+        $user = new \App\Entities\User($this->request->getPost());
+        
+        $model = new \App\Models\UserModel;
+
+        $user->startActivation();
+        
+        if ($model->insert($user)) {
+            
+            $this->sendActivationEmail($user);
+        
+            return redirect()->to("/signup/success");
+            
+        } else {
+            
+            return redirect()->back()
+                             ->with('errors', $model->errors())
+                             ->with('warning', 'Invalid data')
+                             ->withInput();
+        }
+    }
+    
+    public function success()
+    {
+		return view('Signup/success');
+    }
+    
+    private function sendActivationEmail($user)
+    {
+        $email = service('email');
+
+        $email->setTo($user->email);
+
+        $email->setSubject('Account activation');
+
+        $message = view('Signup/activation_email', [
+            'token' => $user->token
+        ]);
+
+        $email->setMessage($message);
+
+        $email->send();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/Entities/User.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Entities;
+
+class User extends \CodeIgniter\Entity
+{
+    public function verifyPassword($password)
+    {    
+        return password_verify($password, $this->password_hash);
+    }
+    
+    public function startActivation()
+    {
+        $this->token = bin2hex(random_bytes(16));
+        
+        $this->activation_hash = hash_hmac('sha256', $this->token, $_ENV['HASH_SECRET_KEY']);
+    }
+}
