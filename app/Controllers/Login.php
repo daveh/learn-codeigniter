10106392ebb6fa623900eb942bb04a6858e36a2b
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 10:46:58 +0100

    Initial commit

---
+++ app/Controllers/Login.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function new()
+    {
+		return view('Login/new');
+    }
+    
+    public function create()
+    {
+		$email = $this->request->getPost('email');
+		$password = $this->request->getPost('password');
+		$remember_me = (bool) $this->request->getPost('remember_me');
+        
+        dd($remember_me);
+        
+        $auth = service('auth');
+        
+        if ($auth->login($email, $password)) {
+            
+            $redirect_url = session('redirect_url') ?? '/';
+            
+			unset($_SESSION['redirect_url']);
+            
+            return redirect()->to($redirect_url)
+                             ->with('info', 'Login successful');
+                             
+        } else {
+            
+            return redirect()->back()
+                             ->withInput()
+                             ->with('warning', 'Invalid login');
+        }
+    }
+    
+    public function delete()
+    {
+        service('auth')->logout();
+
+        return redirect()->to('/login/showLogoutMessage');
+    }
+    
+    public function showLogoutMessage()
+    {
+        return redirect()->to('/')
+                         ->with('info', 'Logout successful');
+    }    
+}
+
+
+
+
+
+
+
