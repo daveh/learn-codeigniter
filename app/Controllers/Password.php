ec165dcd2836c3de68e7a6179eddc75cda90ccdb
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 07:07:17 +0100

    Initial commit

---
+++ app/Controllers/Password.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+class Password extends BaseController
+{
+    public function forgot()
+    {
+        return view('Password/forgot');
+    }
+    
+    public function processForgot()
+    {
+        $model = new \App\Models\UserModel;
+        
+        $user = $model->findByEmail($this->request->getPost('email'));
+        
+        if ($user && $user->is_active) {
+            
+            $user->startPasswordReset();
+            
+            $model->save($user);
+            
+            dd($user);
+            
+        } else {
+            
+            return redirect()->back()
+                             ->with('warning', 'No active user found with that email address')
+                             ->withInput();
+        }
+    }
+}
