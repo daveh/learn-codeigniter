56c08c72157236da19e21a333d4d61dca40f572c
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 06:59:34 +0100

    Initial commit

---
+++ app/Libraries/Authentication.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Libraries;
+
+class Authentication
+{
+    private $user;
+    
+    public function login($email, $password)
+    {
+        $model = new \App\Models\UserModel;
+
+        $user = $model->findByEmail($email);
+                      
+        if ($user === null) {
+            
+            return false;
+            
+        }
+        
+        if ( ! $user->verifyPassword($password)) {
+            
+            return false;
+            
+        }
+        
+        if ( ! $user->is_active) {
+            
+            return false;
+            
+        }
+        
+        $session = session();
+        $session->regenerate();
+        $session->set('user_id', $user->id);
+        
+        return true;
+    }
+    
+    public function logout()
+    {
+        session()->destroy();
+    }
+    
+    public function getCurrentUser()
+    {
+        if ( ! $this->isLoggedIn()) {
+            
+            return null;
+            
+        }
+    
+        if ($this->user === null) {
+        
+            $model = new \App\Models\UserModel;
+            
+            $this->user = $model->find(session()->get('user_id'));
+        }
+        
+        return $this->user;
+    }
+    
+    public function isLoggedIn()
+    {
+        return session()->has('user_id');
+    }
+}
+
+
+
+
+
+
