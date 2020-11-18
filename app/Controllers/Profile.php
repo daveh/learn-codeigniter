83c21994f57eddd0f61ffbf11d31da347aeea356
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 07:13:24 +0100

    Initial commit

---
+++ app/Controllers/Profile.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controllers;
+
+class Profile extends BaseController
+{
+    private $user;
+    
+    public function __construct()
+    {
+        $this->user = service('auth')->getCurrentUser();
+    }
+    
+    public function show()
+	{
+        return view('Profile/show', [
+            'user' => $this->user
+        ]);
+    }
+        
+    public function edit()
+    {
+        return view('Profile/edit', [
+            'user' => $this->user
+        ]);
+    }
+    
+    public function update()
+    {
+        $this->user->fill($this->request->getPost());
+        
+        if ( ! $this->user->hasChanged()) {
+            
+            return redirect()->back()
+                             ->with('warning', 'Nothing to update')
+                             ->withInput();
+        }
+        
+        $model = new \App\Models\UserModel;
+        
+        if ($model->save($this->user)) {
+            
+            return redirect()->to("/profile/show")
+                             ->with('info', 'Details updated successfully');
+        } else {
+            
+            return redirect()->back()
+                             ->with('errors', $model->errors())
+                             ->with('warning', 'Invalid data')
+                             ->withInput();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ app/Views/Signup/activation_email.php
@@ -0,0 +1,5 @@
+<h1>Account activation</h1>
+
+<p>Please click on the link below to activate your account:</p>
+
+<p><a href="<?= site_url("/signup/activate/$token") ?>">Activate account</a></p>
