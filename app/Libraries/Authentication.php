28f240f01f70a5d66f34556be900af4d17b08d53
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 10:50:57 +0100

    Initial commit

---
+++ app/Libraries/Authentication.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Libraries;
+
+class Authentication
+{
+    private $user;
+    
+    public function login($email, $password, $remember_me)
+    {
+        $model = new \App\Models\UserModel;
+
+        $user = $model->findByEmail($email);
+                      
+        if ($user === null) {
+            
+            return false;
+            
+        }
+        
+        if ( ! $user->verifyPassword($password)) {
+            
+            return false;
+            
+        }
+        
+        if ( ! $user->is_active) {
+            
+            return false;
+            
+        }
+        
+        $this->logInUser($user);
+        
+        if ($remember_me) {
+            
+            $this->rememberLogin($user->id);
+            
+        }
+        
+        return true;
+    }
+    
+    private function logInUser($user)
+    {
+        $session = session();
+        $session->regenerate();
+        $session->set('user_id', $user->id);
+    }
+    
+    private function rememberLogin($user_id)
+    {
+        $model = new \App\Models\RememberedLoginModel;
+        
+        list($token, $expiry) = $model->rememberUserLogin($user_id);
+        
+        $response = service('response');
+        
+        $response->setCookie('remember_me', $token, $expiry);
+    }
+    
+    public function logout()
+    {
+        session()->destroy();
+    }
+    
+    private function getUserFromSession()
+    {
+        if ( ! session()->has('user_id')) {
+            
+            return null;
+            
+        }
+        
+        $model = new \App\Models\UserModel;
+        
+        $user = $model->find(session()->get('user_id'));
+        
+        if ($user && $user->is_active) {
+            
+            return $user;
+        }
+    }
+    
+    public function getCurrentUser()
+    {
+        if ($this->user === null) {
+        
+            $this->user = $this->getUserFromSession();
+        }
+        
+        return $this->user;
+    }
+    
+    public function isLoggedIn()
+    {
+        return $this->getCurrentUser() !== null;
+    }
+}
+
+
+
+
+
+
---
+++ app/Views/Password/reset_email.php
@@ -0,0 +1,5 @@
+<h1>Password reset</h1>
+
+<p>Please click on the link below to reset your password:</p>
+
+<p><a href="<?= site_url("/password/reset/$token") ?>">Reset password</a></p>
