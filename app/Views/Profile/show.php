671f7e248e5bc6ee8c701a224a84b3b560bd15b7
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 10:29:07 +0100

    Initial commit

---
+++ app/Views/Profile/show.php
@@ -0,0 +1,23 @@
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('title') ?>Profile<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+<h1>Profile</h1>
+
+<dl>
+    <dt>Name</dt>
+    <dd><?= esc($user->name) ?></dd>
+    
+    <dt>email</dt>
+    <dd><?= esc($user->email) ?></dd>
+</dl>
+
+<a href="<?= site_url("/profile/edit") ?>">Edit</a>
+
+<a href="<?= site_url("/profile/editpassword") ?>">Change password</a>
+
+<a href="<?= site_url("/profileimage/edit") ?>">Change profile image</a>
+
+<?= $this->endSection() ?>
