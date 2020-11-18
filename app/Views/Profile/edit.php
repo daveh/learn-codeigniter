45ce3a5a200da4539c3dc718e97f5ef67b5250c0
Dave Hollingworth <pastelvasco@gmail.com>
2020-11-18 11:10:37 +0100

    Initial commit

---
+++ app/Views/Profile/edit.php
@@ -0,0 +1,34 @@
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('title') ?>Edit profile<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+<h1 class="title">Edit profile</h1>
+
+<?php if (session()->has('errors')): ?>
+    <ul>
+        <?php foreach(session('errors') as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif ?>
+
+<?= form_open("/profile/update") ?>
+
+    <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="<?= old('name', esc($user->name)) ?>">
+    </div>
+
+    <div>
+        <label for="email">email</label>
+        <input type="text" name="email" id="email" value="<?= old('email', esc($user->email)) ?>">
+    </div>    
+    
+    <button>Save</button>
+    <a href="<?= site_url("/profile/show") ?>">Cancel</a>
+
+</form>
+
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/default.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $this->renderSection("title") ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" integrity="sha256-WLKGWSIJYerRN8tbNGtXWVYnUM5wMJTXD8eG4NtGcDM=" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?= site_url('/css/auto-complete.css') ?>">
+</head>
+<body>
+
+<section class="section">
+    
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+
+        <div class="navbar-menu">
+
+            <div class="navbar-start">
+
+                <a class="navbar-item" href="<?= site_url("/") ?>">Home</a>
+                
+            </div>
+
+            <div class="navbar-end">
+
+                <?php if (current_user()): ?>
+                    
+                    <div class="navbar-item">Hello <?= esc(current_user()->name) ?></div>
+                    
+                    <a class="navbar-item" href="<?= site_url("/profile/show") ?>">Profile</a>
+                    
+                    <?php if (current_user()->is_admin): ?>
+                        
+                        <a class="navbar-item" href="<?= site_url("/admin/users") ?>">Users</a>
+                        
+                    <?php endif; ?>
+                    
+                    <a class="navbar-item" href="<?= site_url("/tasks") ?>">Tasks</a>
+                    
+                    <a class="navbar-item" href="<?= site_url("/logout") ?>">Log out</a>
+                    
+                <?php else: ?>
+                    
+                    <a class="navbar-item" href="<?= site_url("/signup") ?>">Sign up</a>    
+                    
+                    <a class="navbar-item" href="<?= site_url("/login") ?>">Log in</a>
+                    
+                <?php endif; ?>
+
+            </div>
+        </div>
+    </nav>
+
+    <?php if (session()->has('warning')): ?>
+        <div class="warning">
+            <?= session('warning') ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (session()->has('info')): ?>
+        <div class="info">
+            <?= session('info') ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (session()->has('error')): ?>
+        <div class="error">
+            <?= session('error') ?>
+        </div>
+    <?php endif; ?>
+
+    <?= $this->renderSection("content") ?>
+    
+</section>
+    
+</body>
+</html>
+
+
+
+
+
+
+
